5a77e8c1b7951260c66cc91096189d168dd5c897
Ugo Palatucci <upalatuc@redhat.com>
2022-05-23 17:40:44 +0200

    buttons with images

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import bluImage from "../assets/blu.jpg";
 import neroImage from "../assets/nero.jpg";
 import verdeImage from "../assets/verde.jpg";
+import verdeButton from "../assets/verde-bottone.png";
+import neroButton from "../assets/nero-bottone.png";
+import bluButton from "../assets/blu-bottone.png";
 
 enum ASSETS {
   BLU = "blu",
@@ -15,6 +18,12 @@ const getAssetFromColor = {
   [ASSETS.VERDE]: verdeImage,
 };
 
+const getButtonFromAsset = {
+  [ASSETS.BLU]: bluButton,
+  [ASSETS.NERO]: neroButton,
+  [ASSETS.VERDE]: verdeButton,
+};
+
 function App() {
   const [color, setColor] = useState(ASSETS.BLU);
 
@@ -23,10 +32,15 @@ function App() {
       <body>
         <div className="buttons">
           {Object.values(ASSETS).map((asset) => (
-            <button onClick={() => setColor(asset)}>{asset}</button>
+            <button onClick={() => setColor(asset)}>
+              {asset}{" "}
+              <img src={getButtonFromAsset[asset]} alt={`button ${asset}`} />
+            </button>
           ))}
         </div>
-        <img src={getAssetFromColor[color]} alt={color} />
+        <div className="main-image">
+          <img src={getAssetFromColor[color]} alt={color} />
+        </div>
       </body>
     </div>
   );
